928e0cb80a86c0adc3bb017d5b98323dddc033da
qywzq <2174907173@qq.com>
2025-02-16 11:44:55 +0800

    测试环境

--- webserve/model/db.js
+++ webserve/model/db.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://admin:secure-password@47.113.204.91:27017/chef?authSource=admin", {
+//生产环境
+// mongoose.connect("mongodb://admin:secure-password@47.113.204.91:27017/chef?authSource=admin", {
+//     maxPoolSize: 10,  // 设置连接池大小
+//     serverSelectionTimeoutMS: 30000 , // 超时设置为 30 秒
+// }).then(() => {
+//     console.log('数据库连接成功')
+// }).catch((err) => {
+//     console.log("数据库连接失败，原因：", err)
+// })
+//测试环境
+mongoose.connect("mongodb+srv://2286893544:3Azf3TzRKXa9bwHR@cluster0.5pee4jn.mongodb.net/chef", {
     maxPoolSize: 10,  // 设置连接池大小
     serverSelectionTimeoutMS: 30000 , // 超时设置为 30 秒
 }).then(() => {
@@ -7,5 +17,4 @@ mongoose.connect("mongodb://admin:secure-password@47.113.204.91:27017/chef?authS
 }).catch((err) => {
     console.log("数据库连接失败，原因：", err)
 })
-
 module.exports = mongoose
